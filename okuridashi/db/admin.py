f8a7f7f16d98116fd6fbe69f1a516bd6a76cb1d8
PaulWestphal <paul.aloys.westphal@outlook.de>
2022-01-01 13:20:31 +0100

    Adds database models to admin.

--- okuridashi/db/admin.py
+++ okuridashi/db/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Bout, Heya, Measurement, Retirement, Rikishi, TournamentDay, Rank, Shikona, Banzuke
 
 # Register your models here.
+admin.site.register(Rikishi)
+admin.site.register(Banzuke)
+admin.site.register(TournamentDay)
+admin.site.register(Shikona)
+admin.site.register(Rank)
+admin.site.register(Measurement)
+admin.site.register(Heya)
+admin.site.register(Retirement)
+admin.site.register(Bout)
